1016f242926db44df87c243ccfd5c36e1c15c7dd
snoutibur <snoutibur@noreply.git.snoutibur.com>
2025-09-27 20:32:41 -0600

    Notes spawn at right place!!

--- Assets/Scripts/NoteSpawner.cs
+++ Assets/Scripts/NoteSpawner.cs
@@ -10,13 +10,15 @@ using UnityEngine;
 public class NoteSpawner : MonoBehaviour {
     public string midiFilePath;
     public GameObject notePrefab;
-    public Transform rootSpawnPoint;
     public AudioSource backingTrack;
     public float approachTime = 3;
 
     private List<ScheduledNote> _notes = new List<ScheduledNote>();
-    private int _nextNoteIndx = 0;
+    private int _nextNoteIndx;
     private double _gameStartTime; // Track when the game actually started
+    
+    // MIDI note -> note coords
+    private Dictionary<int, Transform> _keyLookup = new Dictionary<int, Transform>();
     
     /**
      * This class stores relevant information for spawning notes
@@ -27,9 +29,17 @@ public class NoteSpawner : MonoBehaviour {
         public int NoteID; // MIDI note number
         public float NoteDuration;
     }
-
     
     void Start() {
+        // Build dictionary //
+        foreach (Transform octave in transform) {
+            foreach (Transform key in octave) {
+                if (int.TryParse(key.name, out int midiNote)) {
+                    _keyLookup[midiNote] = key.transform;
+                }
+            }
+        }
+        
         // SETTING UP MIDI FILE //
         // Load midi file
         var mf = MidiFile.Read(midiFilePath);
@@ -64,9 +74,16 @@ public class NoteSpawner : MonoBehaviour {
         if (elapsedTime < 0) return;
 
         while (_nextNoteIndx < _notes.Count && _notes[_nextNoteIndx].SpawnTime <= elapsedTime) {
-            Vector3 noteOffset = new Vector3(0f + _notes[_nextNoteIndx].NoteID, 0f, 0f);
-            Vector3 spawnPos = rootSpawnPoint.position + noteOffset;
-            Instantiate(notePrefab, spawnPos, Quaternion.identity);
+            int targetNote = _notes[_nextNoteIndx].NoteID;
+
+            if (_keyLookup.TryGetValue(targetNote, out Transform keyTransform)) {
+                Vector3 noteOffset = new Vector3(0f, 15f, 0f);
+                Vector3 spawnPos = keyTransform.position + noteOffset;
+                Instantiate(notePrefab, spawnPos, Quaternion.identity);
+            } else {
+                Debug.LogWarning($"Can't find note: {_notes[_nextNoteIndx].NoteID}");
+            }
+            
             _nextNoteIndx++;
         }
     }
